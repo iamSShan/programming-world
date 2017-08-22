784d5ef7bd1c13549156c3156a5131e345189fab
ShantanuSS <shantanusharma197@gmail.com>
2017-08-22 23:36:30 +0530

    amazon ques added

---
+++ Coding Company wise/Amazon_Duplicatesinarray.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class DuplicatesinArray {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,j,cnt=0;
+		int arr[]=new int[n];
+		for( i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+for(i=0;i<n;i++){
+	for(j=i+1;j<n;j++){
+		if(arr[i]==arr[j]){
+			cnt++;
+			System.out.println("elem repeated is "+arr[i]);
+			break;
+		}
+	}
+}
+	System.out.println("no. of repeated elem are"+ cnt);
+	}
+	
+}
