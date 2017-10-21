ce995cb7d2c157146931cb43092742b21235c62e
ShantanuSS <shantanusharma197@gmail.com>
2017-10-21 21:14:31 +0530

    Created a program tp convert octal to decimal

---
+++ Cocubes coding round/OctaltoDecimal.java
@@ -0,0 +1,35 @@
+/*Given, 
+int OctalToDecimal(int n);
+The function takes an integer number, each of whose digits lies between 0 to 7, thus forming an octal number, as input.
+Implement the function to return its decimal equivalent. Write program to convert the octal number to its decimal equivalent as follows:-
+Multiply each digit of the octal number starting with the right most digit and moving leftwards, with increasing powers of 8 starting with 80.
+Example:
+Input:
+127
+
+Output:
+87
+
+Explanation:
+While converting 127 to its decimal equivalent, we start from the right, multiplying 
+7 * 80 = 7
+2 * 81 = 16
+1 * 82 = 64
+
+Adding up the results together, we get 7 + 16 + 64 = 87, which is the output.
+*/
+
+int OctalToDecimal(int n)
+{
+      Int I=0,r,p,s=0;
+      while(n>0)
+{             
+r=n%l0;
+p=pow(8,i);
+s=s+p*r;
+i++;
+n=n/10;
+}
+Return s;
+
+}
